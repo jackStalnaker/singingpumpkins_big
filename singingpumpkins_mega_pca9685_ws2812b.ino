5c6fb5e0d8a759e99353c47b5faddeca74c19bed
Jack Stalnaker <jack.stalnaker@belmont.tech>
2020-10-30 14:53:14 -0500

    update coding style to PEP-8 style (yes, even though it is C++)

--- singingpumpkins_mega_pca9685_ws2812b.ino
+++ singingpumpkins_mega_pca9685_ws2812b.ino
@@ -14,7 +14,7 @@ const bool TESTINGMODE = false;
 
 // Vixen header (identifies a light sequence)
 const int HEADER_LEN = 2;
-const char seqHeader[] = {'~', '!'};
+const char SEQ_HEADER[] = {'~', '!'};
 
 // Timeout waiting for serial input before going to random mode (in milliseconds).
 const int TIME_OUT = 1000;
@@ -52,22 +52,22 @@ const int CHAN12 = 31;
 
 // Servo channels directly controlled by Arduino
 const int NUM_ARD_SERVOS = 0;
-const int ardServoChannels[] = {};
+const int ARD_SERVO_CHANNELS[] = {};
 
 // Servo channels controlled via PCA9685
 const int NUM_PCA_SERVOS = 7;
-const int PCAServoChannels[] = {
+const int PCA_SERVO_CHANNELS[] = {
   PUMPKIN1_MOUTH, PUMPKIN2_MOUTH, PUMPKIN3_MOUTH, PUMPKIN1_FANGS,
   TRUMPET, TRUMPET_ARM, SKULL
 };
 
 // PWM Channels
 const int NUM_PWM = 1;
-const int PWMChannels[] = {BLACKLIGHT};
+const int PWM_CHANNELS[] = {BLACKLIGHT};
 
 // Inverted PWM Channels
 const int NUM_INV_PWM = 9;
-const int invPWMChannels[] = {
+const int INV_PWM_CHANNELS[] = {
   PUMPKIN1_EYES_R, PUMPKIN1_EYES_G, PUMPKIN1_EYES_B,
   PUMPKIN2_EYES_R, PUMPKIN2_EYES_G, PUMPKIN2_EYES_B,
   PUMPKIN3_EYES_R, PUMPKIN3_EYES_G, PUMPKIN3_EYES_B,
@@ -75,7 +75,7 @@ const int invPWMChannels[] = {
 
 // Digital channels
 const int NUM_DIGITAL = 2;
-const int digitalChannels[] = {LIGHTS1, LIGHTS2};
+const int DIGITAL_CHANNELS[] = {LIGHTS1, LIGHTS2};
 
 // UART channels
 const int NUM_UART = 1;
@@ -91,27 +91,27 @@ const int SERVO_DELAY = 15; // delay after servo is activated (allow it to move)
 Servo servos[NUM_ARD_SERVOS];
 
 // Neutral servo position in degrees and pulse length
-const int ardServoNeutral[] = {};
-const int PCAServoNeutral[] = {350, 320, 310, 347, 190, 300, 325};
+const int ARD_SERVO_NEUTRAL[] = {};
+const int PCA_SERVO_NEUTRAL[] = {350, 320, 310, 347, 190, 300, 325};
 
 // Min servo opening in degrees and pulse lengths
-const int ardServoMin[] = {};
-const int PCAServoMin[] = {260, 320, 310, 225, 190, 300, 325};
+const int ARD_SERVO_MIN[] = {};
+const int PCA_SERVO_MIN[] = {260, 320, 310, 225, 190, 300, 325};
 
 // Max servo opening in degrees
-const int ardServoMax[] = {};
-const int PCAServoMax[] = {350, 400, 410, 347, 280, 480, 395};
+const int ARD_SERVO_MAX[] = {};
+const int PCA_SERVO_MAX[] = {350, 400, 410, 347, 280, 480, 395};
 
 // Servo direction
 const int CLOCKWISE = 1;
 const int COUNTERCLOCKWISE = -1;
-const int ardServoDirection[] = {};
-const int PCAServoDirection[] = {COUNTERCLOCKWISE, CLOCKWISE, CLOCKWISE, COUNTERCLOCKWISE, CLOCKWISE, CLOCKWISE, CLOCKWISE};
+const int ARD_SERVO_DIRECTION[] = {};
+const int PCA_SERVO_DIRECTION[] = {COUNTERCLOCKWISE, CLOCKWISE, CLOCKWISE, COUNTERCLOCKWISE, CLOCKWISE, CLOCKWISE, CLOCKWISE};
 
 // Serial
 const long COM_SPEED = 115200;
 const long OUT_COM_SPEED = 4800;
-int incomingByte;
+int incoming_byte;
 
 // PCA9895 driver
 const int SERVO_FREQ = 60;      // PWM frequency for servos
@@ -123,19 +123,19 @@ CRGB leds[NUM_LED];
 // PA driven skull
 const int PA_SKULL = 45;
 const int PA_MIC = 49;
-const int micReadDelay = 25;
-const int servoMoveDelay = 50;
-Servo paSkullServo;
-const int paSkullMin = 0;
-const int paSkullMax = 40;
-int micVal = 0;
-unsigned long micReadTimer = 0;
-unsigned long servoMoveTimer = 0;
+const int PA_MIC_READ_DELAY = 25;
+const int PA_SERVO_MOVE_DELAY = 50;
+const int PA_SKULL_MIN = 0;
+const int PA_SKULL_MAX = 30;
+Servo pa_skull_servo;
+int mic_val = 0;
+unsigned long mic_read_timer = 0;
+unsigned long servo_move_timer = 0;
 
 // Misc
-int i = 0;                              // Loop counter
-int j = 0;                              // Loop counter
-volatile unsigned long  timer_a = 0;    // Timer
+int i = 0;                                  // Loop counter
+int j = 0;                                  // Loop counter
+volatile unsigned long last_msg_time = 0;   // Timer
 
 
 //setup the pins/ inputs & outputs
@@ -158,39 +158,39 @@ void setup()
   // initalize channels and set to starting state
   for (i = 0; i < NUM_ARD_SERVOS; i++) {
     if (TESTINGMODE) {
-      pinMode(ardServoChannels[i], OUTPUT);
-      digitalWrite(ardServoChannels[i], LOW);
+      pinMode(ARD_SERVO_CHANNELS[i], OUTPUT);
+      digitalWrite(ARD_SERVO_CHANNELS[i], LOW);
     } else {
-      servos[i].attach(ardServoChannels[i]);
-      servos[i].write(ardServoNeutral[i]);
+      servos[i].attach(ARD_SERVO_CHANNELS[i]);
+      servos[i].write(ARD_SERVO_NEUTRAL[i]);
     }
   }
 
   for (i = 0; i < NUM_PCA_SERVOS; i++) {
     if (TESTINGMODE) {
-      pwm.setPin(PCAServoChannels[i], 0);
+      pwm.setPin(PCA_SERVO_CHANNELS[i], 0);
     } else {
-      pwm.setPin(PCAServoChannels[i], PCAServoNeutral[i]);
+      pwm.setPin(PCA_SERVO_CHANNELS[i], PCA_SERVO_NEUTRAL[i]);
     }
   }
 
   for (i = 0; i < NUM_PWM; i++) {
-    pinMode(PWMChannels[i], OUTPUT);
-    digitalWrite(PWMChannels[i], LOW);
+    pinMode(PWM_CHANNELS[i], OUTPUT);
+    digitalWrite(PWM_CHANNELS[i], LOW);
   }
 
   for (i = 0; i < NUM_INV_PWM; i++) {
-    pinMode(invPWMChannels[i], OUTPUT);
+    pinMode(INV_PWM_CHANNELS[i], OUTPUT);
     if (TESTINGMODE) {
-      digitalWrite(invPWMChannels[i], LOW);
+      digitalWrite(INV_PWM_CHANNELS[i], LOW);
     } else {
-      digitalWrite(invPWMChannels[i], HIGH);
+      digitalWrite(INV_PWM_CHANNELS[i], HIGH);
     }
   }
 
   for (i = 0; i < NUM_DIGITAL; i++) {
-    pinMode(digitalChannels[i], OUTPUT);
-    digitalWrite(digitalChannels[i], LOW);
+    pinMode(DIGITAL_CHANNELS[i], OUTPUT);
+    digitalWrite(DIGITAL_CHANNELS[i], LOW);
   }
 
   for (i = 0; i < NUM_UART; i++) {
@@ -205,8 +205,8 @@ void setup()
 
   // Initialize PA skull
   pinMode(PA_MIC, INPUT);
-  paSkullServo.attach(PA_SKULL);
-  paSkullServo.write(paSkullMin);
+  pa_skull_servo.attach(PA_SKULL);
+  pa_skull_servo.write(PA_SKULL_MIN);
 
   test_sequence(); // brief test
   Serial.begin(COM_SPEED);   // set up input serial
@@ -216,12 +216,12 @@ void loop()
 {
   if (Serial.available() > 0) {
     //wdt_reset(); // resets the watchdog (prevents board lockup)
-    timer_a = millis ();  // Mark the time when a message was received
+    last_msg_time = millis ();  // Mark the time when a message was received
 
     // read the header to verify this is in fact a light sequence
     for (int i = 0; i < HEADER_LEN; i++) {
       while (!Serial.available());
-      if (seqHeader[i] != Serial.read()) {
+      if (SEQ_HEADER[i] != Serial.read()) {
         return;
       }
     }
@@ -229,13 +229,13 @@ void loop()
     // Arduino servos
     for (i = 0; i < NUM_ARD_SERVOS; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
+      incoming_byte = Serial.read();
       if (TESTINGMODE) {
-        analogWrite(ardServoChannels[i], incomingByte);
+        analogWrite(ARD_SERVO_CHANNELS[i], incoming_byte);
       } else {
-        int angle = map(incomingByte, 0, 255, ardServoMin[i], ardServoMax[i]);
-        angle *= ardServoDirection[i];
-        servos[i].write(ardServoNeutral[i] + angle);
+        int angle = map(incoming_byte, 0, 255, ARD_SERVO_MIN[i], ARD_SERVO_MAX[i]);
+        angle *= ARD_SERVO_DIRECTION[i];
+        servos[i].write(ARD_SERVO_NEUTRAL[i] + angle);
         delay(SERVO_DELAY);
       }
     }
@@ -243,53 +243,53 @@ void loop()
     // PCA9895 servos
     for (i = 0; i < NUM_PCA_SERVOS; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
+      incoming_byte = Serial.read();
       if (TESTINGMODE) {
-        int pulselen = map(incomingByte, 0, 255, 0, 4095);
-        pwm.setPin(PCAServoChannels[i], pulselen);
+        int pulselen = map(incoming_byte, 0, 255, 0, 4095);
+        pwm.setPin(PCA_SERVO_CHANNELS[i], pulselen);
       } else {
-        if (PCAServoDirection[i] == COUNTERCLOCKWISE) {
-          incomingByte = 255 - incomingByte;
+        if (PCA_SERVO_DIRECTION[i] == COUNTERCLOCKWISE) {
+          incoming_byte = 255 - incoming_byte;
         }
-        int pulselen = map(incomingByte, 0, 255, PCAServoMin[i], PCAServoMax[i]);
-        pwm.setPin(PCAServoChannels[i], pulselen);
+        int pulselen = map(incoming_byte, 0, 255, PCA_SERVO_MIN[i], PCA_SERVO_MAX[i]);
+        pwm.setPin(PCA_SERVO_CHANNELS[i], pulselen);
       }
     }
 
     // PWM
     for (i = 0; i < NUM_PWM; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
-      analogWrite(PWMChannels[i], incomingByte);
+      incoming_byte = Serial.read();
+      analogWrite(PWM_CHANNELS[i], incoming_byte);
     }
 
     // Inverted PWM
     for (i = 0; i < NUM_INV_PWM; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
+      incoming_byte = Serial.read();
       if (TESTINGMODE) {
-        analogWrite(invPWMChannels[i], incomingByte);
+        analogWrite(INV_PWM_CHANNELS[i], incoming_byte);
       } else {
-        analogWrite(invPWMChannels[i], 255 - incomingByte);
+        analogWrite(INV_PWM_CHANNELS[i], 255 - incoming_byte);
       }
     }
 
     // Digital
     for (i = 0; i < NUM_DIGITAL; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
-      if (incomingByte <= 127) {
-        digitalWrite(digitalChannels[i], LOW);
+      incoming_byte = Serial.read();
+      if (incoming_byte <= 127) {
+        digitalWrite(DIGITAL_CHANNELS[i], LOW);
       } else {
-        digitalWrite(digitalChannels[i], HIGH);
+        digitalWrite(DIGITAL_CHANNELS[i], HIGH);
       }
     }
 
     // UART
     for (i = 0; i < NUM_UART; i++) {
       while (!Serial.available());
-      incomingByte = Serial.read();
-      Serial1.write(incomingByte);
+      incoming_byte = Serial.read();
+      Serial1.write(incoming_byte);
     }
 
     // LED strip
@@ -308,27 +308,27 @@ void loop()
   } else {
     // Random mode starts if no serial input has been received in TIME_OUT milliseconds
     //wdt_reset(); // resets the watchdog (prevents board lockup)
-    unsigned long diff = millis() - timer_a;
-    if (diff >= TIME_OUT) {
-      timer_a = millis ();
+    unsigned long time_without_msg = millis() - last_msg_time;
+    if (time_without_msg >= TIME_OUT) {
+      last_msg_time = millis ();
 
       if (TESTINGMODE) {
         for (i = 0; i < NUM_ARD_SERVOS; i++) {
-          random_light(ardServoChannels[i]);
+          random_light(ARD_SERVO_CHANNELS[i]);
         }
         for (i = 0; i < NUM_PCA_SERVOS; i++) {
-          random_light_pca(PCAServoChannels[i]);
+          random_light_pca(PCA_SERVO_CHANNELS[i]);
         }
       }
 
       for (i = 0; i < NUM_PWM; i++) {
-        random_light(PWMChannels[i]);
+        random_light(PWM_CHANNELS[i]);
       }
       for (i = 0; i < NUM_INV_PWM; i++) {
-        random_light(invPWMChannels[i]);
+        random_light(INV_PWM_CHANNELS[i]);
       }
       for (i = 0; i < NUM_DIGITAL; i++) {
-        random_light(digitalChannels[i]);
+        random_light(DIGITAL_CHANNELS[i]);
       }
       random_strip();
     }
@@ -336,14 +336,14 @@ void loop()
   
   // PA skull
   unsigned long now = millis();
-  if (((now - micReadTimer) > micReadDelay) &&
-      ((now - servoMoveTimer) > servoMoveDelay)) {
-    micVal = digitalRead(PA_MIC);
-    if (micVal) {
-      paSkullServo.write(paSkullMax);
-      servoMoveTimer = millis();
+  if (((now - mic_read_timer) > PA_MIC_READ_DELAY) &&
+      ((now - servo_move_timer) > PA_SERVO_MOVE_DELAY)) {
+    mic_val = digitalRead(PA_MIC);
+    if (mic_val) {
+      pa_skull_servo.write(PA_SKULL_MAX);
+      servo_move_timer = millis();
     } else {
-      paSkullServo.write(paSkullMin);
+      pa_skull_servo.write(PA_SKULL_MIN);
     }
   }
 }
@@ -352,21 +352,21 @@ void loop()
 void test_sequence() {
   if (TESTINGMODE) {
     for (i = 0; i < NUM_ARD_SERVOS; i++) {
-      test_channel(ardServoChannels[i]);
+      test_channel(ARD_SERVO_CHANNELS[i]);
     }
     for (i = 0; i < NUM_PCA_SERVOS; i++) {
-      test_channel_pca(PCAServoChannels[i]);
+      test_channel_pca(PCA_SERVO_CHANNELS[i]);
     }
   }
 
   for (i = 0; i < NUM_PWM; i++) {
-    test_channel(PWMChannels[i]);
+    test_channel(PWM_CHANNELS[i]);
   }
   for (i = 0; i < NUM_INV_PWM; i++) {
-    test_channel(invPWMChannels[i]);
+    test_channel(INV_PWM_CHANNELS[i]);
   }
   for (i = 0; i < NUM_DIGITAL; i++) {
-    test_channel(digitalChannels[i]);
+    test_channel(DIGITAL_CHANNELS[i]);
   }
 
   for (i = 0; i < NUM_LED; i++) {
